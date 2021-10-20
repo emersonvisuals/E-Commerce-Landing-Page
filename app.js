801b7c7a5ff0fbb14669c28e825a6b797193689e
Thomas Emerson <emersonvisuals@gmail.com>
2021-10-20 12:14:46 +1100

    collapsible menu complete

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ const burgerMenu = document.querySelector('div.hamburgerMenu img');
 const burgerMenuActive = document.querySelector('div.links');
 const close = document.querySelector('.exit img');
 const backdrop = document.querySelector('.backdrop');
+const body = document.body; 
 
 
 burgerMenu.addEventListener('click', function(){
     burgerMenuActive.classList.add('active');
     backdrop.classList.add('active');
-    console.log('clicked');
+    body.classList.add('active');
 });
 
 close.addEventListener('click', function(){
     burgerMenuActive.classList.remove('active');
     backdrop.classList.remove('active');
+    body.classList.remove('active');
 });
 
 backdrop.addEventListener('click', function(){
     burgerMenuActive.classList.remove('active');
     backdrop.classList.remove('active');
+    body.classList.remove('active');
 });
 
